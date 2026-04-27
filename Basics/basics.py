74f35d4c444c0d573022ba25b049607c7305ace6
Amarworks <notfoundreal404@gmail.com>
2026-04-27 15:13:20 +0530

    Added Basics

---
+++ Basics/basics.py
@@ -0,0 +1,9 @@
+print("HELLO VIEWERS")
+
+print(100/3) #you can use * for multiplication % for remainder etc.. 
+
+print("Hey I am Amar Kant Diwakar\nand i am author of this repository. ")
+
+print("You can also check my \"profile\".  ") #you can also use ' ' instead of " " without using \ \..
+
+print("Date",27,4,2026, sep="-", end=".\n") 
